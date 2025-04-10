d8a5b4bb3d382d6bce85b8b4f54824eac39126d6
Tolemy <tolemy.kashyap@1mg.com>
2025-04-10 13:32:02 +0530

    fix: added connect section

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import * as Animatable from 'react-native-animatable';
 import { usePathname } from "expo-router";
 import Menu from "./menu";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Home() {
 
@@ -15,7 +16,7 @@ export default function Home() {
                 padding: 20,
             }}
         >
-            <View style={{ marginBottom: 30,flexDirection: isMenuOpen? 'column': 'row', alignItems: 'flex-start', height: 100, justifyContent: 'flex-start' }}>
+            <View style={{ marginBottom: 30,flexDirection: isMenuOpen? 'column': 'row', alignItems: 'flex-start', height: 80, justifyContent: 'flex-start' }}>
                 <TouchableOpacity onPress={() => { setIsMenuOpen(!isMenuOpen)}} style={{justifyContent: 'flex-start'}}>
                     <Text style={{fontSize: 17}}>Menu </Text>
                 </TouchableOpacity>
@@ -30,7 +31,7 @@ export default function Home() {
                         <Text style={{fontSize: 75, fontWeight: 800, color: 'green'}}>Tolemy Kashyap</Text>
                     </Animatable.View>
                     <View style={{padding: 15, marginBottom: 10}}>
-                        <Text style={{fontWeight:800,marginTop:20, marginBottom: 15}}>
+                        <Text style={{fontWeight:800,marginTop:10, marginBottom: 10}}>
                             Full Stack Developer | Tech Enthusiast
                         </Text>
                         <Text>
@@ -38,14 +39,33 @@ export default function Home() {
                             Let's build something great together 🚀
                         </Text>
                     </View>
-                    <Animatable.View animation="pulse" iterationCount="infinite" duration={1000} style={{alignItems: 'center', marginBottom:50}}>
+                    <Animatable.View animation="pulse" iterationCount="infinite" duration={1000} style={{alignItems: 'center'}}>
                         <TouchableOpacity
                             onPress={()=>{Linking.openURL('https://drive.google.com/file/d/1qlY7IFo6bqzGTOiv_m4ES12IrrL6BmTe/view?usp=sharing')}}
-                            style={{marginTop: 10, alignItems: 'center', padding:10, borderWidth: 1, borderColor: 'green', borderRadius: 8, backgroundColor: '#fff7ed'}}
+                            style={{marginBottom:25, alignItems: 'center', padding:10, borderWidth: 1, borderColor: 'green', borderRadius: 8, backgroundColor: '#fff7ed'}}
                         >
                             <Text style={{color:'green',fontWeight:800,}}>Download Resume</Text>
                         </TouchableOpacity>
                     </Animatable.View>
+                    <View style={{marginBottom: 50,padding: 15}}>
+                        <View style={{marginBottom: 10}}>
+                            <Text style={{fontSize:15,color:'#024c69' }}>Find me on: </Text>
+                        </View>
+                        <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+                            <TouchableOpacity onPress={()=> Linking.openURL('https://www.linkedin.com/in/tolemy-kashyap/')}>
+                                <AntDesign name="linkedin-square" size={24} color="#33691d" />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => Linking.openURL('https://github.com/KashyapTolemy')}>
+                                <AntDesign name="github" size={24} color="#33691d" />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => Linking.openURL('https://medium.com/@kashyaptolemy03')}>
+                                <AntDesign name="medium-monogram" size={24} color="#33691d" />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={()=>Linking.openURL('https://www.instagram.com/tolemy_kashyap/')}>
+                                <AntDesign name="instagram" size={24} color="#33691d" />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
             </ScrollView>
         </View>
